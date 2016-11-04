2e39d674259a690ca175eeaea5cfbffd1e9ed3d1
KenichiroHohda <snggtd@gmail.com>
2016-11-04 14:25:00 +0000

    オリジナル比較表（サンプル）

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103123106) do
+ActiveRecord::Schema.define(version: 20161103140742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,19 @@ ActiveRecord::Schema.define(version: 20161103123106) do
     t.float    "sodium"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "comp_name"
+    t.string   "prd_id"
+    t.string   "jan"
+    t.string   "prd_name"
+    t.integer  "price"
+    t.string   "category"
+    t.string   "prd_img"
+    t.datetime "sales_release"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
