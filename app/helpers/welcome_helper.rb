33b82e7dfa32a8c24ba68ddac5a9ca0f141f3ed5
tantan5 <jojo1938043@gmail.com>
2016-11-13 13:28:03 +0000

    不要な表示項目削除。商品に枠を追加。商品IDを追加。

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,27 +1,11 @@
 module WelcomeHelper
   def print_product(target)
-     content_tag(:div, :class => 'product', :id => 'accordion'+target.shop) do
-       concat content_tag(:a, target.name,:class => 'p_name', :"data-toggle" => 'collapse', :"data-parent" => '#accordion'+target.shop, :href => '#collapseOne'+target.shop)
+     content_tag(:div, :class => 'product', :id => 'accordion'+target.id.to_s) do
+       concat content_tag(:a, target.shop+"  "+target.name,:class => 'p_name', :"data-toggle" => 'collapse', :"data-parent" => '#accordion'+target.shop, :href => '#collapseOne'+target.shop) 
        concat (content_tag(:div, :class => 'spec', :id => 'collapseOne'+target.shop) do 
 	 concat (content_tag(:dl ,:class => 'dl-horizontal') do
 	    concat content_tag(:dt,"税込価格")
 	    concat content_tag(:dd,target.price.to_s + "円")
-	    concat content_tag(:dt,"熱量")
-	    concat content_tag(:dd,target.calorie.to_s+"kcal")
-	    concat content_tag(:dt,"分量")
-	    concat content_tag(:dd,target.net.to_s+"g")
-	    concat content_tag(:dt,"タンパク質")
-	    concat content_tag(:dd,target.protein.to_s+"g")
-	    concat content_tag(:dt,"脂質")
-	    concat content_tag(:dd,target.fat.to_s+"g")
-	    concat content_tag(:dt,"炭水化物")
-	    concat content_tag(:dd,target.carbon.to_s+"g")
-	    concat content_tag(:dt,"糖質")
-	    concat content_tag(:dd,target.sugar.to_s+"g")
-	    concat content_tag(:dt,"食物繊維")
-	    concat content_tag(:dd,target.fiber.to_s+"g")
-	    concat content_tag(:dt,"ナトリウム")
-	    concat content_tag(:dd,target.sodium.to_s+"mg")
 	 end)
        end)
      end
--- app/models/product_dto.rb
+++ app/models/product_dto.rb
@@ -1,21 +1,13 @@
 class Product_dto 
   include ActiveModel::Model
   
-  attr_accessor :shop,:category,:item,:name,:price,:calorie,:net,:protein,:fat,:carbon,:sugar,:fiber,:sodium,:tweets_count_all,:tweets_count_good,:tweets_count_bad
+  attr_accessor :id,:shop,:category,:item,:name,:price,:calorie,:net,:protein,:fat,:carbon,:sugar,:fiber,:sodium,:tweets_count_all,:tweets_count_good,:tweets_count_bad
   
-  def initialize(shop,category,item,name,price,calorie,net,protein,fat,carbon,sugar,fiber,sodium)
+  def initialize(id,shop,category,name,price)
+    @id=id
     @shop=shop
     @category=category
-    @item=item
     @name=name
     @price=price
-    @calorie=calorie
-    @net=net
-    @protein=protein
-    @fat=fat
-    @carbon=carbon
-    @sugar=sugar
-    @fiber=fiber
-    @sodium=sodium
   end
 end
