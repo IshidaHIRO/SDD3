410a0363283d430425fb078b331956f1d7bff766
tantan5 <jojo1938043@gmail.com>
2016-11-14 15:40:47 +0000

    Herokuで画像が表示されるよう修正

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -3,7 +3,7 @@ module WelcomeHelper
      content_tag(:div, :class => 'product', :id => 'accordion'+target.id.to_s) do
        concat content_tag(:a, target.shop+"  "+target.name,:class => 'p_name', :"data-toggle" => 'collapse', :"data-parent" => '#accordion'+target.shop, :href => '#collapseOne'+target.shop) 
        concat (content_tag(:div, :class => 'spec', :id => 'collapseOne'+target.shop) do 
-	 concat image_tag('assets/default01.jpg', :width => '200',:style => 'float :left')
+	 concat image_tag('/assets/default01.jpg', :width => '200',:style => 'float :left')
          concat (content_tag(:dl) do
 	    concat content_tag(:dt,"税込価格")
 	    concat content_tag(:dd,target.price.to_s + "円")
