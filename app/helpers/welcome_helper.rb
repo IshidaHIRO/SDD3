4b05f606313ddaaf098617d878f8f3b149eb4f49
tantan5 <jojo1938043@gmail.com>
2016-11-16 11:55:44 +0000

    商品詳細への導線を分かりやすくするため、画像及びリンク用テキストを追加

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,13 +1,15 @@
 module WelcomeHelper
   def print_product(target)
+     link = '/items/view/'+target.name+'?shop='+target.shop
      content_tag(:div, :class => 'product', :id => 'accordion'+target.id.to_s) do
-       concat content_tag(:a, target.shop+"  "+target.name,:class => 'p_name', :href => '/items/view/'+target.name+'?shop='+target.shop) 
+       concat content_tag(:a, target.shop+"  "+target.name,:class => 'p_name', :href => link) 
        concat (content_tag(:div, :class => 'spec', :id => 'collapseOne'+target.shop) do 
-	 concat image_tag('/images/default01.jpg', :width => '200',:style => 'float :left')
+	 concat content_tag(:a,image_tag('/images/default01.jpg', :width => '200',:style => 'float :left'),:href => link)
          concat (content_tag(:dl) do
 	    concat content_tag(:dt,"税込価格")
 	    concat content_tag(:dd,target.price.to_s + "円")
 	 end)
+         concat link_to('商品の詳細を見る',link)
        end)
      end
    end
