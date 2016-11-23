fe0102de190e00830974c6ec7b8e78834417eb44
tantan5 <jojo1938043@gmail.com>
2016-11-23 02:41:03 +0000

    コメント一覧の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,8 @@ class ItemsController < ApplicationController
   # GET /items/1
   # GET /items/1.json
   def show
+    #口コミ情報の取得
+    @item_reviews=ItemReview.where(item_id:@item.id)
   end
 
   def vote
