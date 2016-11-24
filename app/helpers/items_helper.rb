0991f27d9b6ca177dc70e93dcb1f9a8680a6cf80
tantan5 <jojo1938043@gmail.com>
2016-11-24 11:09:56 +0000

    商品情報部分が狭い場合はGood/Badボタンが縦並びになるように変更

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -9,19 +9,19 @@ module ItemsHelper
            else
              concat image_tag('/images/'+item.image, :width => "150",:style => "float :left")
            end
-           concat (content_tag(:dl) do
-             concat content_tag(:dt,"税込価格")
-             concat content_tag(:dd,item.price.to_s + "円")
-             concat content_tag(:dt,"話題度")
-             concat content_tag(:dd,item.good + item.bad)
-             concat content_tag(:dt,"good/bad")
-             concat content_tag(:dd,item.good.to_s+"/"+item.bad.to_s)
-           end)
-             concat (content_tag(:div, :style => "margin-left :160px") do 
-               concat hidden_field(:item,:id,value:item.id)
-               concat submit_tag("GOOD",:name =>"item[good]",:class => "btn btn-lg btn-primary")
-               concat submit_tag("BAD",:name =>"item[bad]",:class => "btn btn-lg btn-danger",:style =>"margin-left:20px")
+           concat (content_tag(:div) do
+             concat (content_tag(:dl) do
+               concat content_tag(:dt,"税込価格")
+               concat content_tag(:dd,item.price.to_s + "円")
+               concat content_tag(:dt,"話題度")
+               concat content_tag(:dd,item.good + item.bad)
+               concat content_tag(:dt,"good/bad")
+               concat content_tag(:dd,item.good.to_s+"/"+item.bad.to_s)
              end)
+             concat hidden_field(:item,:id,value:item.id)
+             concat submit_tag("GOOD",:name =>"item[good]",:class => "btn btn-lg btn-primary")
+             concat submit_tag("BAD",:name =>"item[bad]",:class => "btn btn-lg btn-danger")
+           end)
          end)
        end
      end
